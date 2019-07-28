2c3fa7435083493b4faae3d73f959f95a2a4980a
nathanielam0ah <nkp.amoah@gmail.com>
2019-07-28 23:12:02 +0000

    Improved Readability, + login method in ATM class, created ATM object.

--- bank3.py
+++ bank3.py
@@ -1,54 +1,45 @@
 #!/usr/bin/env python
-main_code = 1234
+
 #uml classes
-print("welcome to Python Sandwich Bank ATM")
-while True:
-    code  = int(input("please enter your code: "))
-    if code == main_code:
-        input("select your transaction")
-        input1 = input("Press 1 for Balance: ")
-        input2 = input("Press 2 for withdraw: ")
-        input3 = input("Press 3 for Deposit: ")
-        input4 = input("press 4 for Transfer Transaction: ")
-        
-        list = [input1, input2, input3]
-        
-        switch (list)
-            case list[0]:
-                balance = Account("saving", "customer")
-                balance.checkbalance()
-                break
-            case list[1]:
-                withdraw = withdrawal_transaction(904)
-                withdraw.withdraw()
-                break
-            case list[3]:
-                transfer = Transfer_Transaction()
-                transfer.amount = 56
-                transfer.accountNo = 40110495837262
-                print(transfer.amount)
-                print(transfer.accountNo)
-                break
-            
-        
-        
-        
-
-
 #parent classes
 class Bank:
-    code = None
-    address = None
+    code = "PSW2019GRP02"
+    address = "AITILAB03"
+    accountNo = "GRP0002"
+    accountPin = 1234
+
     def manages(self):
         print("this is method manages under bank class")
+
     def maintains(self):
         print("this is the maintains method under the Bank class")
 
 class ATM:
     location = None
     managedBy = None
+
+    def __init__(self, accountNo, accountPin):
+        self.accountNo = accountNo
+        self.accountPin = accountPin
+
+    def logIn(self):
+        tries = 0
+        while tries < 4:
+            self.accountNo = str(input("ACCOUNT ID: "))
+            self.accountPin = int(input("PIN: "))
+
+            if (self.accountNo == Bank.accountNo) and (self.accountPin == Bank.accountPin):
+                print("SUCCESS")
+                return True
+            else:
+                print("INCORRECT LOGIN INFO, TRY AGAIN")
+                tries += 1
+
+            return False
+
     def identifies(self):
         print("identifies method under ATM class")
+
     def transactions(self):
         print("transaction method under ATM class")
 
@@ -64,26 +55,25 @@ class withdrawal_transaction(atmTransaction):
     # pin = 1234
     def __init__(self, amount):
         self.amount = amount
-    
-    def withdrawal(self, amount, balance ):
-         if self.amount <=  balance:
+
+    def withdrawal(self, amount, balance):
+        if self.amount <= balance:
             new_balance = balance - amount
             print(new_balance)
         else:
-            print("sorry! insufficient balance")
-            
+            print("INSUFFICIENT BALANCE")
+
 class Transfer_Transaction(atmTransaction):
     def __init__(self, amount,accountNo):
         self.amount = amount
         self.accountNo = accountNo
-    
-    
-    
+
 
 #in association with Bank class
 class debitCard:
     cardNo = None
     ownedBy = None
+
     def access(self):
         print("this is the debit Card class")
 
@@ -91,32 +81,39 @@ class Customer:
     name = None
     address = None
     dob = None
+
     def owns(self):
         print("this is the customer class")
-        
+
 class Account:
     balance  = 2000
+
     def __init__(self, types,owner):
-        self.types
-        self.owner
-    
+        self.types = "savings"
+        self.owner = "group02"
+
     def checkBalance(self):
         print(Balance)
-        
-    
-    
 
-#in association debit card class and customer class and the ATM transaction class.
+
+#in direct association debit card class and customer class and the ATM transaction class.
 class savingAccounts:
     accountNo = None
+
     def debit(self):
         print("print this is the savings account class")
+
     def credit(self):
         print("this is the checkingAccount class")
 
 class checkingAccount:
     accountNo = None
+
     def debit(self):
         print("this is the checkingAccount class")
+
     def credit(self):
         print("this is the checkingAccount class")
+
+group02ATM = ATM("GRP0002", 1234)
+group02ATM.logIn()
