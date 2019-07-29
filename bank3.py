2479c0e367409af93e63ef24ab2d2d5124dc280e
nathanielam0ah <nkp.amoah@gmail.com>
2019-07-29 12:48:41 +0000

    +menu, +login and +logout function. added some funtions under.

--- bank3.py
+++ bank3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #uml classes
-#parent classes
+#assuming bank as database
 class Bank:
     firstName = "group"
     lastName = "02"
@@ -19,6 +19,7 @@ class Bank:
 class ATM:
     location = None
     managedBy = None
+    logIn_confirmation = None
 
     def __init__(self, accountNo, accountPin):
         self.accountNo = accountNo
@@ -32,12 +33,17 @@ class ATM:
 
             if (self.accountNo == Bank.accountNo) and (self.accountPin == Bank.accountPin):
                 print("LOGIN SUCCESS")
-                return True
+                ATM.logIn_confirmation = True
+                return ATM.logIn_confirmation
             else:
                 print("INCORRECT LOGIN INFO, TRY AGAIN")
                 tries += 1
 
-            return False
+                ATM.logIn_confirmation = False
+                return ATM.logIn_confirmation
+
+    def logOut(self):
+        exit()
 
     def identifies(self):
         print("identifies method under ATM class")
@@ -45,9 +51,35 @@ class ATM:
     def transactions(self):
         print("transaction method under ATM class")
 
+    def mainMenu(self):
+        menuChoice = None
+
+        if ATM.logIn_confirmation == True:
+            while 1:
+                print("WELCOME TO PSW2019 GROUP02 BANK ATM.")
+                print("ENTER 1 TO CHECK ACCOUNT BALANCE, 2 TO DEPOSIT AND 3 TO WITHDRAW, 4 TO LOG OUT")
+                menuChoice = input()
+
+                if (menuChoice == 1):
+                    testAccount = Account("savings", "group02")
+                    testAccount.checkBalance()
+                    break
+                elif (menuChoice == 2):
+                    testAccount = transfer_Transaction()
+                    testAccount.deposit()
+                    break
+                elif (menuChoice == 3):
+                    testAccount = withdrawal_transaction()
+                    testAccount.withdrawal()
+                    break
+                elif (menuChoice == 4):
+                    ATM.logIn()
+        if ATM.logIn_confirmation == False:
+            exit()
+
 #under atm class
 class atmTransaction:
-    transactionId = None
+    transactionID = None
     date = None
     type = None
     def update(self):
@@ -58,17 +90,23 @@ class withdrawal_transaction(atmTransaction):
     def __init__(self, amount):
         self.amount = amount
 
-    def withdrawal(self, amount, balance):
-        if self.amount <= balance:
-            new_balance = balance - amount
+    def withdrawal(self, amount):
+        self.amount = input("AMOUNT: ")
+        if self.amount <= Account.balance:
+            new_balance = Account.balance - amount
             print(new_balance)
         else:
             print("INSUFFICIENT BALANCE")
 
-class Transfer_Transaction(atmTransaction):
+class transfer_Transaction(atmTransaction):
     def __init__(self, amount,accountNo):
         self.amount = amount
         self.accountNo = accountNo
+
+    def deposit(self):
+        self.amount = float(input("AMOUNT: "))
+        Account.balance = Account.balance + amount
+        print("BALANCE: ", Account.balance)
 
 
 #in association with Bank class
@@ -80,22 +118,22 @@ class debitCard:
         print("this is the debit Card class")
 
 class Customer:
-    name = None
-    address = None
-    dob = None
+    name = Bank.firstName + " " + Bank.lastName
+    address = Bank.address
+    dob = "15-07-19"
 
     def owns(self):
         print("this is the customer class")
 
 class Account:
-    balance  = 2000
+    balance  = 00.0
 
     def __init__(self, types,owner):
         self.types = "savings"
         self.owner = "group02"
 
     def checkBalance(self):
-        print(Balance)
+        print("BALANCE: ", Account.balance)
 
 
 #in direct association debit card class and customer class and the ATM transaction class.
@@ -119,3 +157,4 @@ class checkingAccount(Account):
 
 group02ATM = ATM("GRP0002", 1234)
 group02ATM.logIn()
+group02ATM.mainMenu()
